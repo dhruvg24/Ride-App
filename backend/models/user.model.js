c8e226b63bb21ffb2f25fe99d0c109fb9e07d982
dhruvg24 <dhruvgarg24112002@gmail.com>
2025-06-05 19:35:57 +0530

    user model created

---
+++ backend/models/user.model.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jwt')
+const module = require('module')
+
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String, 
+            required: true, 
+            minlength: [3, 'First name should be atleast 3 characters long'],
+        },
+        lastname: {
+            type: String, 
+            minlength: [3, 'Last name should be atleast 3 characters long']
+        }
+    },
+    email:{
+        type: String,
+        required: true, 
+        unique: true,
+        minlength: [5, 'Email must be atleast 5 characters long']
+    },
+    password: {
+        type: String, 
+        required: true,
+        select: false
+    }, 
+    socketId: {
+        type: String
+    }
+});
+
+userSchema.methods.generateAuthToken=function(){
+    const token=jwt.sign({_id: this._id}, process.env.JWT_SECRET)
+    return token;
+}
+
+userSchema.methods.comparePasswords=async function(pswd){
+    return await bcrypt.compare(pswd, this.password)
+}
+
+userSchema.statics.hashPassword=async function(pswd){
+    return await bcrypt.hash(pswd, 10);
+}
+
+const userModel=mongoose.model('user', userSchema);
+
+module.exports = userModel;
+
+
+
